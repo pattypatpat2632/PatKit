ac9d0f29c85542090f6dfc03ca4be15aecab636e
pattypatpat2632 <oleary.audio@gmail.com>
2017-09-20 17:04:17 -0400

    Initial Commit

---
+++ PatKit/PatKit.h
@@ -0,0 +1,19 @@
+//
+//  PatKit.h
+//  PatKit
+//
+//  Created by Patrick O'Leary on 9/20/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PatKit.
+FOUNDATION_EXPORT double PatKitVersionNumber;
+
+//! Project version string for PatKit.
+FOUNDATION_EXPORT const unsigned char PatKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PatKit/PublicHeader.h>
+
+
